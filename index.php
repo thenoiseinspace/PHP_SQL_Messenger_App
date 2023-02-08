9c6bd35b591eff91fc411d80ed065b1e45457aa8
Sarah Gilbreath <thenoiseinspace@gmail.com>
2023-02-08 10:46:20 -0500

    initial index layout for login

---
+++ index.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+<title>ChatBox</title>
+<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<div id="main">
+    <br><br><br>
+    <h4>Manage your team messages</h4>
+    <img src="icon.png" width="100px"/> <br> 
+
+<table>
+    <form method = "post" action="#">
+
+    <input type="text" name="username" placeholder="Username"><br>
+    <input type="password" name="password" placeholder="Password"><br>
+    <button type="submit" name="login">Log in</button>
+
+    </form>
+</table>
+</div>
+
+<div id="footer">
+    All rights reserved. Copyright 2023.<?php echo date('Y');?>
+</div>
+</body>
+
+<?php ?>
+</body>
+</html>
